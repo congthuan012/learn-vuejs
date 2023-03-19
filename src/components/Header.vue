e3b1e819b115f58f639befa595f08f10bfc7b35d
Cong Thuan <thuan.huynh@take1.vn>
2023-03-19 20:14:29 +0700

    setup

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+      <header class="header">
+            <div class="header-item" id="right">
+                  <div id="bar"><i class="fa-solid fa-bars"></i></div>
+                  <div id="web-name">Learn VueJs</div>
+            </div>
+            <form action="">
+                  <div class="header-item">
+                        <input type="text" class="search-input">
+                        <button class="search-button"><i class="fa-solid fa-magnifying-glass"></i></button>
+                  </div>
+            </form>
+            <div class="header-item">
+                  <div id="profile-item">
+                        <span class="profile-name">Thuan</span>
+                  </div>
+                  <img src="../assets/images/profile.webp" alt="" class="profile-avatar">
+            </div>
+      </header>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-import './assets/main.css'
-
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import './assets/style.css';
+import '../public/fontawesome-free-6.3.0-web/css/all.css';
 createApp(App).mount('#app')
